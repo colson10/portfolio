35245fac3e9822df5141b8a0525966e9387042ce
Carl Olson <colson10@gmail.com>
2018-08-03 13:22:08 -0700

    changed webpack entry back

--- webpack.common.js
+++ webpack.common.js
@@ -4,9 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const webpackConfig = module.exports = {};
 
-webpackConfig.entry = {
-  app: `${__dirname}/src/main.js`,
-};
+webpackConfig.entry = `${__dirname}/src/main.js`;
 
 webpackConfig.output = {
   filename: '[name].[hash].js',
