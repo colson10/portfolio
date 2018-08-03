772f22c420287898817c159e66e19e2929272040
Carl Olson <colson10@gmail.com>
2018-08-03 16:02:47 -0700

    favicon added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Favicon from 'react-favicon';
 import { Helmet } from 'react-helmet';
 import Landing from '../landing/landing';
-
-// import Favicon from 'react-favicon';
 
 import '../../style/main.scss';
 
@@ -11,7 +10,8 @@ export default class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-      <div>
+        <div>
+          <Favicon url='https://avatars1.githubusercontent.com/u/33847838?s=460&v=4'/>
           <Helmet>
             <meta name='viewport' content='width=device-width, initial-scale=1.0' />
             <meta property="og:title" content="Carl Olson Portfolio" />
